bd00532ce6a7ba628149eab5b4b6bf9f618f5ef9
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-10-17 15:02:44 +0900

    sanitized

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # aws-apac
+This module provisions a EC2. also those peripherals to make a connection ready.
+
+>[!note]
+>Declare your credential as environment variables before you run.
+>`export EQUINIX_API_CLIENTID=someEquinixAPIClientID`
+>`export EQUINIX_API_CLIENTSECRET=someEquinixAPIClientSecret`
+>`export METAL_AUTH_TOKEN=someEquinixMetalToken`
+>
+> AWS access key can be same way. refer https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html?icmpid=docs_sso_user_portal
--- main.tf
+++ main.tf
@@ -2,56 +2,30 @@ terraform {
   required_providers {
 
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.63.1"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region = var.aws_region
 }
 
 
-data "terraform_remote_state" "AWS_VPC_id" {
+data "terraform_remote_state" "AWS_outputs" {
   backend = "remote"
 
   config = {
     organization = "EQIX_projectX"
     workspaces = {
-      name = "VipinTestRepo"
-    }
-  }
-}
-
-data "terraform_remote_state" "AWS_Subnet_id" {
-  backend = "remote"
-
-  config = {
-    organization = "EQIX_projectX"
-    workspaces = {
-      name = "VipinTestRepo"
-    }
-  }
-}
-
-data "terraform_remote_state" "AWS_VPG_id" {
-  backend = "remote"
-
-  config = {
-    organization = "EQIX_projectX"
-    workspaces = {
-      name = "VipinTestRepo"
+      name = "network-builder-apac"
     }
   }
 }
 
 module "aws-components" {
-   source                = "git::github.com/Eqix-ProjectX/terraform-equinix-virtualconnection-aws-components"
-   aws_vpc_id = data.terraform_remote_state.AWS_VPC_id.outputs.AWS_VPC_id
-   aws_subnet_id = data.terraform_remote_state.AWS_Subnet_id.outputs.AWS_Subnet_id
-   aws_vpg_id           = data.terraform_remote_state.AWS_VPG_id.outputs.AWS_VPG_id
- 
+  source        = "github.com/Eqix-ProjectX/terraform-equinix-virtualconnection-aws-components/"
+  aws_vpc_id    = data.terraform_remote_state.AWS_outputs.outputs.AWS_VPC_id
+  aws_subnet_id = data.terraform_remote_state.AWS_outputs.outputs.AWS_Subnet_id
+  aws_vpg_id    = data.terraform_remote_state.AWS_outputs.outputs.AWS_VPG_id
 }
