3e6a1c13337cb0613be94d79b33f62d7268e072d
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-10-23 13:45:47 +0900

    propagation added on vgw

--- main.tf
+++ main.tf
@@ -29,3 +29,8 @@ module "aws-components" {
   aws_subnet_id = data.terraform_remote_state.AWS_outputs.outputs.AWS_Subnet_id
   aws_vpg_id    = data.terraform_remote_state.AWS_outputs.outputs.AWS_VPG_id
 }
+
+resource "aws_vpn_gateway_route_propagation" "for_eq" {
+  vpn_gateway_id = data.terraform_remote_state.AWS_outputs.outputs.AWS_VPG_id
+  route_table_id = module.aws-components.route_table_id
+}
